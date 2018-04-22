96751554d98eef4734fd22ae0bce3a76146f7543
alex <alexsharron@gmail.com>
2018-04-22 15:21:21 -0400

    Made the gensis block possess blog-content and blog-title

--- blockchain/genesis.py
+++ blockchain/genesis.py
@@ -3,4 +3,4 @@ from block import Block
 
 def create_genesis_block():
     #Create beginning block with random value
-    return Block(0, date.datetime.now(), {'proof-of-work':9,'transactions':0}, "0")
+    return Block(0, date.datetime.now(), {'blog-title':'Gensis Block','blog-content':'This is the genesis block','proof-of-work':9,'transactions':0}, "0")
